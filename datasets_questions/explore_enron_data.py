f8455c36df2c05b2f1f87552668e02b8fc326a3b
Manav <manavm@utexas.edu>
2015-07-05 21:01:08 -0400

    Added code for feature selection and validation

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -16,9 +16,31 @@
 """
 
 import pickle
+import math
+import numpy as np
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
-print enron_data["SKILLING JEFFREY K"]["exercised_stock_options"]
+print enron_data["LAY KENNETH L"]
+
+
+total_pois = 0
+missing_payments = 0
+for person in enron_data.keys():
+	if enron_data[person]["poi"] == True:
+		total_pois += 1
+		if enron_data[person]["total_payments"] == "NaN":
+			missing_payments += 1
+print missing_payments
+
+# salary_count = 0
+# email_count = 0
+# for person in enron_data.keys():
+# 	if enron_data[person]["email_address"] != "NaN":
+# 		email_count += 1
+# 	if enron_data[person]["salary"] != "NaN":
+# 		salary_count += 1
+# print email_count, salary_count
+
 
 # total_pois = 0
 # for person in enron_data.keys():
